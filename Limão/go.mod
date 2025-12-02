5ef53e6fb622aa909ef8e62e36c684b289c43328
Zelchi <tininhakawaii@gmail.com>
2025-12-02 01:54:11 -0300

    refactor: simplify main function and update dependencies

---
+++ Limão/go.mod
@@ -0,0 +1,5 @@
+module lemon
+
+go 1.25.4
+
+require github.com/zelchi/testego v1.0.4
--- Limão/main.go
+++ Limão/main.go
@@ -1,55 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"github.com/zelchi/testego"
+)
 
 func main() {
-
-	pessoa := Pessoa{
-		Nome:   "Batata",
-		Idade:  90,
-		Altura: 1.34,
-	}
-
-	cachorro := Cachorro{
-		Apelido: "Cenoura",
-		Cor:     "Branca",
-	}
-
-	pessoa.Apresentacao()
-	cachorro.Apresentacao()
-
-	var apresentador Apresentador = &pessoa
-
-	apresentador.Apresentacao()
-
-	ReceberApresentador(apresentador)
-}
-
-type Pessoa struct {
-	Nome   string
-	Idade  int
-	Altura float64
-}
-
-type Cachorro struct {
-	Apelido string
-	Cor     string
-}
-
-type Apresentador interface {
-	Apresentacao()
-}
-
-func (pessoa *Pessoa) Apresentacao() {
-	fmt.Printf("Olá meu nome é %v, tenho %v de idade e %v de altura.",
-		pessoa.Nome, pessoa.Idade, pessoa.Altura)
-}
-
-func (pessoa *Cachorro) Apresentacao() {
-	fmt.Printf("O nome do cachorro é %v, ele tem a cor %v.",
-		pessoa.Apelido, pessoa.Cor)
-}
-
-func ReceberApresentador(alvo Apresentador) {
-	fmt.Println("\nA pessoa alvo é ", alvo)
+	fruta := testego.CreateFruit("Batata", 20.50)
+	testego.Start(fruta)
+	fruta.Selling()
+	fruta.Marketing()
 }
